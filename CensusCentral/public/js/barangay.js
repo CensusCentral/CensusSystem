1fb6dd5ecf64343ec1a19b017fe3aa43488deb08
JoshuaPermejo <permejojoshua57@gmail.com>
2024-08-26 15:43:56 +0800

    add barangay section

---
+++ CensusCentral/public/js/barangay.js
@@ -0,0 +1,40 @@
+
+	// Select/Deselect checkboxes
+	var checkbox = $('table tbody input[type="checkbox"]');
+	$("#selectAll").click(function(){
+		if(this.checked){
+			checkbox.each(function(){
+				this.checked = true;                        
+			});
+		} else{
+			checkbox.each(function(){
+				this.checked = false;                        
+			});
+		} 
+	});
+	checkbox.click(function(){
+		if(!this.checked){
+			$("#selectAll").prop("checked", false);
+		}
+	});
+
+	// Filter table rows based on searched term
+    $(document).ready(function(){
+        // Activate tooltips
+        $('[data-toggle="tooltip"]').tooltip();
+        
+        // Filter table rows based on searched term
+        $("#search").on("keyup", function() {
+            var term = $(this).val().toLowerCase();
+            $("table tbody tr").each(function(){
+                $row = $(this);
+                var name = $row.find("td:nth-child(2)").text().toLowerCase();
+                console.log(name);
+                if(name.search(term) < 0){                
+                    $row.hide();
+                } else{
+                    $row.show();
+                }
+            });
+        });
+    });
