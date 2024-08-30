ec24622ce743e2231709decd795b4092d5fe8cfe
JoshuaPermejo <permejojoshua57@gmail.com>
2024-08-30 22:02:09 +0800

    Update dashboard.blade.php
    space lang

--- CensusCentral/resources/views/dashboard/dashboard.blade.php
+++ CensusCentral/resources/views/dashboard/dashboard.blade.php
@@ -9,6 +9,7 @@
     <link rel="stylesheet" href="https://maxst.icons8.com/vue-static/landings/line-awesome/line-awesome/1.3.0/css/line-awesome.min.css">
 </head>
 <body>
+    
    <input type="checkbox" id="menu-toggle">
     <div class="sidebar">
         <div class="side-header">
